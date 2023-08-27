671806e32034d4e11b7d809befb6a2a57c85fdd9
student <student@Ubuntu20.myguest.virtualbox.org>
2023-08-27 18:06:06 +0300

    add flask-app

---
+++ flask-app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+# list of cat images
+images = [
+        "https://i.gifer.com/2GU.gif",
+        "https://i.gifer.com/3QeI.gif",
+        "https://i.gifer.com/59ER.gif",
+        "https://i.gifer.com/7GUP.gif"
+            ]
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html', url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
